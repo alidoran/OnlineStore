752a5c929bd1818fefb8c07bf732b8f30476ae94
alidoran <alidoran@users.noreply.github.com>
2019-02-06 14:48:34 +0330

    modified:   app/src/main/java/alidoran/ir/OnlineStore/ASyncLike.java

--- app/src/main/java/alidoran/ir/OnlineStore/ASyncLike.java
+++ app/src/main/java/alidoran/ir/OnlineStore/ASyncLike.java
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-//package alidoran.ir.OnlineStore;
-//
-//import android.os.AsyncTask;
-//import java.io.BufferedReader;
-//import java.io.InputStreamReader;
-//import java.io.OutputStreamWriter;
-//import java.net.URL;
-//import java.net.URLConnection;
-//import java.net.URLEncoder;
-//
-//public class ASyncLike extends AsyncTask {
-//
-//    public String link = "";
-//    public String user_id="";
-//
-//    public ASyncLike ( String link , String user_id ) {
-//        this.link = link;
-//        this.user_id=user_id;
-//    }
-//
-//
-//    @Override
-//    protected Object doInBackground ( Object[] objects ) {
-//        try {
-//            //user & pass convert to URL
-//            String data= URLEncoder.encode("userid","utf8")+"="+URLEncoder.encode ( user_id , "utf8"  );
-//
-//            //connect to server
-//            URL url = new URL ( link );
-//            URLConnection connection = url.openConnection ( );
-//
-//            //send to server
-//            connection.setDoOutput ( true );
-//            OutputStreamWriter streamWriter =new OutputStreamWriter ( connection.getOutputStream () );
-//            streamWriter.write ( data );
-//            streamWriter.flush ();
-//
-//            //read data
-//            BufferedReader reader = new BufferedReader ( new InputStreamReader ( connection.getInputStream ( ) ) );
-//            StringBuilder builder = new StringBuilder ( );
-//
-//            String line = null;
-//
-//            while ((line = reader.readLine ( )) != null) {
-//                builder.append ( line );
-//            }
-//            Comment.test = builder.toString ( );
-//
-//        } catch (Exception error) {
-//        }
-//        return "";
-//    }
-//}
-=======
 package alidoran.ir.OnlineStore;
 
 import android.os.AsyncTask;
@@ -106,4 +51,3 @@ public class ASyncLike extends AsyncTask {
         return "";
     }
 }
->>>>>>> 7d7e776ea0fe67a26e8111f52ccd81881aa5be5c
